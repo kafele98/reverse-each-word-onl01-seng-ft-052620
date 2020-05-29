12413eb075a0fd160a4d1161a01fbd30c7d189dd
Zoe Keller <keller.zoe98@gmail.com>
2020-05-29 17:50:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence1)
-  sentence1_reversed = sentence1.split(" ").reverse.join(" ")
+  sentence1_reversed = sentence1.split(" ").reverse!.join(" ")
   puts sentence1_reversed
 end 
 
