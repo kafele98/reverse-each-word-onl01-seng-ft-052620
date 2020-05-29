5749e8099ac5d41c3907d21dc2deca73b2d542c5
Zoe Keller <keller.zoe98@gmail.com>
2020-05-29 17:44:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,5 @@
 def reverse_each_word(sentence1)
-#takes in a string argument 
-#returns same sentence with each word in reversed place 
-#bro you gotta turn the string into an array 
-    array= [] 
-     array<< sentence1.split 
-     
-    reverse_statement=[ ]
-    array 
-       array.each do |word|
-       reverse_statement.push "#{word}"
-    end
-  reverse_statement
+  string 
 end 
+
+puts reverse_each_word(sentence1)
